7b05cb91bb24a402a98e99bb099bc1b6fcb309c6
이지현 <jihyunlee@ijihyeon-ui-MacBook-Pro.local>
2017-10-28 21:20:32 +0900

    add graphic editor

--- helloworld/helloworld/GraphicEditor.c
+++ helloworld/helloworld/GraphicEditor.c
@@ -7,3 +7,234 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LENGTH 100
+
+int gCanvas[MAX_LENGTH][MAX_LENGTH];
+
+
+
+void initCanvas (){
+    int i;
+    
+    for ( i = 0 ; i < MAX_LENGTH ;  ++i )
+    {
+        memset ( gCanvas[i], 0x0 , MAX_LENGTH);
+    }
+}
+
+void showCanvas( int M, int N ){
+    
+    
+    //printf("[%d %s]\n", __LINE__, __func__);
+    for ( int i = 0 ; i < M; ++i ){
+        for( int j = 0 ; j < N ; ++j )
+        {
+            printf("%c", gCanvas[i][j]);
+        }
+        printf("\n");
+    }
+    
+    
+}
+void processI(int row, int column ){
+    
+    for ( int i = 0 ; i < row; ++i ){
+        for( int j = 0 ; j < column ; ++j )
+        {
+            //gCanvas[i][j] = 'O';
+            gCanvas[i][j] = '-';
+        }
+    }
+    showCanvas(row, column);
+}
+
+void clearCanvas (int row, int column){
+    
+    processI(row, column);
+    
+}
+
+void drawApixel( int x, int y, char color ){
+    gCanvas[y-1][x-1] = color;
+}
+
+void drawVerticalLine( int x, int y1, int y2, char color){
+    
+    for (int i = y1-1 ; i < y2 ; ++ i ){
+        gCanvas[i][x-1] = color;
+    }
+
+}
+void drawHorizontalLine( int x1, int x2, int y, char color){
+    
+    for (int i = x1-1 ; i < x2 ; ++ i ){
+        gCanvas[y-1][i] = color;
+    }
+}
+    
+void drawRectangular( int x1, int y1, int x2, int y2, char color){
+
+    drawHorizontalLine(x1, x2, y1, color);
+    drawHorizontalLine(x1, x2, y2, color);
+    
+    drawVerticalLine(x1, y1 ,y2, color);
+    drawVerticalLine(x2, y1 ,y2, color);
+}
+
+
+
+void FillTheColor( int x, int y , char color, int row, int column ){
+    
+    char posColor;
+
+    if ( !(y-1 >= 0 && y-1 < row && x-1 >=0 && x-1 < column))
+        return;
+
+    //current [ y-1 , x-1 ]
+    posColor = gCanvas[y-1][x-1];
+    
+    //check up
+    gCanvas[y-1][x-1]=color;
+    if ( y-2 >= 0 ){
+        
+        if ( gCanvas[y-2][x-1] == posColor ){
+            FillTheColor(x, y-1, color, row, column );
+            gCanvas[y-2][x-1]=color;
+        }
+    }
+    
+    //check down
+    if ( y  < row ){
+        if ( gCanvas[y][x-1] == posColor ){
+            FillTheColor(x, y+1, color, row, column );
+            gCanvas[y][x-1]=color;
+        }
+    }
+
+    //check left
+    if( x-2  >= 0 ){
+        if ( gCanvas[y-1][x-2] == posColor ){
+            FillTheColor(x-1, y, color, row, column );
+            gCanvas[y-1][x-2]=color;
+        }
+    }
+
+    //check right
+    if ( x < column ){
+        if ( gCanvas[y-1][x] == posColor ){
+            FillTheColor(x+1, y, color, row, column );
+            gCanvas[y-1][x]=color;
+        }
+    }
+
+    
+    
+}
+
+int main(){
+
+    //1. receive command from user
+    //2. process command accordding to first character.
+    //3. go to step 1.
+    char cmd;
+    int isEnd=0;
+    int row=0;
+    int column=0;
+    printf("Graphic Editor\n");
+    initCanvas();
+    
+    
+    while ( isEnd != 1 )
+    {
+
+        scanf("%c", &cmd);
+        switch(cmd){
+            case 'I':
+            {
+                scanf("%d %d", &column , &row);
+                printf("raw, column [%d %d]\n", row, column);
+                processI(row, column);
+            }
+                break;
+            case 'C':
+            {
+                clearCanvas(row, column);
+            }
+                break;
+            case 'L':
+            {
+                int x,y;
+                char color;
+                scanf("%d %d %c", &x, &y, &color);
+                printf("L x y c [%d %d %c]\n", x, y, color);
+                drawApixel( x, y , color);
+                showCanvas( row, column );
+            }
+                break;
+                
+            case 'V':
+            {
+                int x,y1,y2;
+                char color;
+                scanf("%d %d %d %c", &x, &y1, &y2, &color);
+                drawVerticalLine(x, y1, y2, color);
+                //debug
+                showCanvas( row, column );
+            }
+                break;
+            case 'H':
+            {
+                int x1,x2,y;
+                char color;
+                scanf("%d %d %d %c", &x1, &x2, &y, &color);
+                drawHorizontalLine(x1, x2, y, color);
+                //debug
+                showCanvas( row, column );
+            }
+                break;
+            case 'K':
+            {
+                int x1,x2,y1 ,y2;
+                char color;
+                scanf("%d %d %d %d %c", &x1, &y1, &x2, &y2, &color);
+                drawRectangular(x1, y1, x2, y2, color);
+                //debug
+                showCanvas( row, column );
+            }
+                break;
+            case 'F':
+            {
+                int x,y;
+                char color;
+                scanf("%d %d %c", &x, &y, &color);
+                printf("F x y c [%d %d %c]\n", x, y, color);
+                FillTheColor( x, y , color , row, column);
+                showCanvas( row, column );
+                
+            }
+                break;
+            case 'S':
+            {
+                char fileName[15]={0,};
+                scanf("%s",fileName);
+                showCanvas( row, column );
+            }
+                break;
+            case 'X':
+                isEnd=1;
+            default:;
+                printf("default\n");
+        }
+        
+        getchar();
+        
+    }
+    
+    printf("terminate program\n");
+    
+    
+    return 0;
+}
--- helloworld/helloworld/LCDDisplay.c
+++ helloworld/helloworld/LCDDisplay.c
@@ -7,3 +7,466 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_SIZE      100000
+char gLedPannel[MAX_SIZE][MAX_SIZE];
+
+
+int receiveInputToDisplay( int * size, char * numberToDisplay ){
+    
+    printf("input size and number to be displayed : ");
+    scanf("%d %s",size , numberToDisplay);
+    printf("size : %d number : %s\n", *size, numberToDisplay );
+    
+    if ( *size == 0 && !strcmp(numberToDisplay,"0") )
+        return -1;
+    return 0;
+}
+
+//raw       2s+3
+//column    s+2
+//ex1)     (5,3) , (7,4)
+
+#if 1
+char dashLine   = '-';
+char rightBar   = '|';
+char leftBar    = '|';
+char whiteSpace = ' ';
+#else//easy to think considering right and left.
+char * dashLine  = "--";
+char * rightBar   = " |";
+char * leftBar    = "| ";
+char * whiteSpace = "  ";
+#endif
+void drawOne( int size, int pos ){
+    
+    int i,j;
+    int raw = 2 * size + 3;
+    int column= size + 2;
+    int relativePos = (size + 2 )*pos;
+
+    //printf("[%d,%d]\n",raw , column);
+    for ( i = 0 ; i < raw  ; ++i ){
+        
+        for ( j = 0 ; j < column ; ++j ){
+            if ( i % (size + 1) == 0 ){
+                gLedPannel[i][j + relativePos]=whiteSpace;//printf("%s", whiteSpace );
+                continue;
+            }
+
+            if ( j == column-1 )
+                gLedPannel[i][j + relativePos]=rightBar; //printf("%s", rightBar);
+            else
+                gLedPannel[i][j + relativePos]=whiteSpace; //printf("%s", whiteSpace);
+        }
+        //printf("\n");
+        
+    }
+}
+void drawEight( int size , int pos){
+    
+    int i,j;
+    int raw = 2 * size + 3;
+    int column= size + 2;
+    int relativePos = (size + 2 )*pos;
+    
+    //printf("[%d,%d]\n",raw , column);
+    for ( i = 0 ; i < raw  ; ++i ){
+        
+        for ( j = 0 ; j < column ; ++j ){
+            if ( i % (size + 1) == 0 ){
+                
+                if ( (j == column-1) || j == 0 ){
+                    gLedPannel[i][j + relativePos]=whiteSpace;// printf("%s", whiteSpace );
+                } else {
+                    gLedPannel[i][j + relativePos]=dashLine;//  printf("%s", dashLine );
+                }
+                continue;
+            }
+            
+            if ( j == 0 )
+                gLedPannel[i][j + relativePos]=rightBar; //printf("%s", rightBar);
+            else if (j == column-1)
+                gLedPannel[i][j + relativePos]=leftBar; //printf("%s", leftBar);
+            else
+                gLedPannel[i][j + relativePos]=whiteSpace; //printf("%s", whiteSpace);
+        }
+        //printf("\n");
+        
+    }
+}
+
+void drawFour( int size , int pos){
+    
+    int i,j;
+    int raw = 2 * size + 3;
+    int column= size + 2;
+    int relativePos = (size + 2 )*pos;
+    
+    //printf("[%d,%d]\n",raw , column);
+    for ( i = 0 ; i < raw  ; ++i ){
+        
+        for ( j = 0 ; j < column ; ++j ){
+            if ( i % (size + 1) == 0 ){
+                
+                if ( (j == column-1) || j == 0 ){
+                    gLedPannel[i][j + relativePos]=whiteSpace;//printf("%s", whiteSpace );
+                }
+                else if( i == 0 || i == raw - 1 ){
+                    gLedPannel[i][j + relativePos]=whiteSpace;//printf("%s", whiteSpace );
+                }
+                else {
+                    gLedPannel[i][j + relativePos]=dashLine;//printf("%s", dashLine );
+                }
+                continue;
+            }
+            
+            if ( j == 0 && i < raw/2 )
+                gLedPannel[i][j + relativePos]=rightBar; //printf("%s", rightBar);
+            else if (j == column-1)
+                gLedPannel[i][j + relativePos]=leftBar; //printf("%s", leftBar);
+            else
+                gLedPannel[i][j + relativePos]=whiteSpace;//printf("%s", whiteSpace);
+        }
+        //printf("\n");
+        
+    }
+}
+
+void drawTwo( int size, int pos ){
+    
+    int i,j;
+    int raw = 2 * size + 3;
+    int column= size + 2;
+    int relativePos = (size + 2 )*pos;
+    
+    //printf("[%d,%d]\n",raw , column);
+    for ( i = 0 ; i < raw  ; ++i ){
+        
+        for ( j = 0 ; j < column ; ++j ){
+            if ( i % (size + 1) == 0 ){
+                
+                if ( (j == column-1) || j == 0 ){
+                    gLedPannel[i][j + relativePos]=whiteSpace;//printf("%s", whiteSpace );
+                } else{
+                    gLedPannel[i][j + relativePos]=dashLine;//printf("%s", dashLine );
+                }
+                continue;
+            }
+            
+            if ( j == 0 && i > raw/2 )
+                gLedPannel[i][j + relativePos]=rightBar; //printf("%s", rightBar);
+            else if (j == column-1 && i < raw/2 )
+                gLedPannel[i][j + relativePos]=leftBar; //printf("%s", leftBar);
+            else
+               gLedPannel[i][j + relativePos]=whiteSpace ; //printf("%s", whiteSpace);
+        }
+        //printf("\n");
+        
+    }
+}
+void drawNine( int size , int pos ){
+    
+    int i,j;
+    int raw = 2 * size + 3;
+    int column= size + 2;
+    int relativePos = (size + 2 )*pos;
+    
+    //printf("[%d,%d]\n",raw , column);
+    for ( i = 0 ; i < raw  ; ++i ){
+        
+        for ( j = 0 ; j < column ; ++j ){
+            if ( i % (size + 1) == 0 ){
+                
+                if ( (j == column-1) || j == 0 ){
+                    gLedPannel[i][j + relativePos]=whiteSpace;//printf("%s", whiteSpace );
+                }
+                else if( i == raw - 1 ){
+                    gLedPannel[i][j + relativePos]=whiteSpace;//printf("%s", whiteSpace );
+                }
+                else {
+                    gLedPannel[i][j + relativePos]=dashLine; //printf("%s", dashLine );
+                }
+                continue;
+            }
+            
+            if ( j == 0 && i < raw/2 )
+                gLedPannel[i][j + relativePos]=rightBar; //printf("%s", rightBar);
+            else if (j == column-1)
+                gLedPannel[i][j + relativePos]=leftBar; //printf("%s", leftBar);
+            else
+                gLedPannel[i][j + relativePos]=whiteSpace;// printf("%s", whiteSpace);
+        }
+        //printf("\n");
+        
+    }
+}
+
+
+void drawFive( int size ,int pos){
+    
+    int i,j;
+    int raw = 2 * size + 3;
+    int column= size + 2;
+    int relativePos = (size + 2 )*pos;
+    
+    //printf("[%d,%d]\n",raw , column);
+    for ( i = 0 ; i < raw  ; ++i ){
+        
+        for ( j = 0 ; j < column ; ++j ){
+            if ( i % (size + 1) == 0 ){
+                
+                if ( (j == column-1) || j == 0 ){
+                    gLedPannel[i][j + relativePos]=whiteSpace;//printf("%s", whiteSpace );
+                } else{
+                    gLedPannel[i][j + relativePos]=dashLine;//printf("%s", dashLine );
+                }
+                continue;
+            }
+            
+            if ( j == 0 && i < raw/2 )
+                gLedPannel[i][j + relativePos]=rightBar;//printf("%s", rightBar);
+            else if (j == column-1 && i > raw/2 )
+                gLedPannel[i][j + relativePos]=leftBar;//printf("%s", leftBar);
+            else
+                gLedPannel[i][j + relativePos]=whiteSpace;//printf("%s", whiteSpace);
+        }
+        //printf("\n");
+        
+    }
+}
+void drawSix( int size ,int pos){
+    
+    int i,j;
+    int raw = 2 * size + 3;
+    int column= size + 2;
+    int relativePos = (size + 2 )*pos;
+    
+    //printf("[%d,%d]\n",raw , column);
+    for ( i = 0 ; i < raw  ; ++i ){
+        
+        for ( j = 0 ; j < column ; ++j ){
+            if ( i % (size + 1) == 0 ){
+                
+                if ( (j == column-1) || j == 0 ){
+                    gLedPannel[i][j + relativePos]=whiteSpace;//printf("%s", whiteSpace );
+                } else{
+                    gLedPannel[i][j + relativePos]=dashLine;//printf("%s", dashLine );
+                }
+                continue;
+            }
+            
+            if ( j == 0 )
+                gLedPannel[i][j + relativePos]=rightBar;//printf("%s", rightBar);
+            else if (j == column-1 && i > raw/2 )
+                gLedPannel[i][j + relativePos]=leftBar;//printf("%s", leftBar);
+            else
+                gLedPannel[i][j + relativePos]=whiteSpace;//88printf("%s", whiteSpace);
+        }
+        //printf("\n");
+        
+    }
+}
+void drawThree( int size ,int pos){
+    
+    int i,j;
+    int raw = 2 * size + 3;
+    int column= size + 2;
+    int relativePos = (size + 2 )*pos;
+    
+    //printf("[%d,%d]\n",raw , column);
+    for ( i = 0 ; i < raw  ; ++i ){
+        
+        for ( j = 0 ; j < column ; ++j ){
+            if ( i % (size + 1) == 0 ){
+                
+                if ( (j == column-1) || j == 0 ){
+                    gLedPannel[i][j + relativePos]=whiteSpace;//printf("%s", whiteSpace );
+                } else {
+                    gLedPannel[i][j + relativePos]=dashLine;//printf("%s", dashLine );
+                }
+                continue;
+            }
+            
+            if ( j == 0 )
+                gLedPannel[i][j + relativePos]=whiteSpace;//printf("%s", whiteSpace);
+            else if (j == column-1 )
+                gLedPannel[i][j + relativePos]=leftBar;//printf("%s", leftBar);
+            else
+                gLedPannel[i][j + relativePos]=whiteSpace; //printf("%s", whiteSpace);
+        }
+        //printf("\n");
+        
+    }
+}
+void drawSeven( int size ,int pos){
+    
+    int i,j;
+    int raw = 2 * size + 3;
+    int column= size + 2;
+    int relativePos = (size + 2 )*pos;
+    
+    //printf("[%d,%d]\n",raw , column);
+    for ( i = 0 ; i < raw  ; ++i ){
+        
+        for ( j = 0 ; j < column ; ++j ){
+            if ( i % (size + 1) == 0 ){
+                
+                if ( (j == column-1) || j == 0 || i != 0 ){
+                    gLedPannel[i][j + relativePos]=whiteSpace;//printf("%s", whiteSpace );
+                } else {
+                    gLedPannel[i][j + relativePos]=dashLine;//printf("%s", dashLine );
+                }
+                continue;
+            }
+            
+            if ( j == 0 )
+                gLedPannel[i][j + relativePos]=whiteSpace;//printf("%s", whiteSpace);
+            else if (j == column-1 )
+                gLedPannel[i][j + relativePos]=leftBar;// printf("%s", leftBar);
+            else
+                gLedPannel[i][j + relativePos]=whiteSpace;//printf("%s", whiteSpace);
+        }
+        //printf("\n");
+        
+    }
+}
+
+
+void drawZero( int size , int pos ){
+    
+    int i,j;
+    int raw = 2 * size + 3;
+    int column= size + 2;
+    int relativePos = (size + 2 )*pos;
+    
+    //printf("[%d,%d]\n",raw , column);
+    for ( i = 0 ; i < raw  ; ++i ){
+        
+        for ( j = 0 ; j < column ; ++j ){
+            if ( i % (size + 1) == 0 ){
+                
+                if ( (j == column-1) || j == 0 ){
+                    //printf("%s", whiteSpace );
+                    gLedPannel[i][j + relativePos]=whiteSpace;
+                } else if (i == 0 || i == raw -1 ) {
+                    //printf("%s", dashLine );
+                     gLedPannel[i][j + relativePos]=dashLine;
+                }
+                else {
+                    //printf("%s", whiteSpace );
+                    gLedPannel[i][j + relativePos]=whiteSpace;
+                }
+                continue;
+            }
+            
+            if ( j == 0 )
+                gLedPannel[i][j + relativePos]=rightBar; //printf("%s", rightBar);
+            else if (j == column-1)
+                gLedPannel[i][j + relativePos]=leftBar; //printf("%s", leftBar);
+            else
+                gLedPannel[i][j + relativePos]=whiteSpace;//printf("%s", whiteSpace);
+        }
+        //printf("\n");
+        
+    }
+
+}
+
+
+void initLed(){
+    
+    int i;
+    for ( i = 0; i < MAX_SIZE ; ++i )
+    {
+        memset( gLedPannel[i], 0x00, MAX_SIZE );
+    }
+    
+}
+
+void drawNumbersToLed(int size, char * numberToDisplay ){
+    int i;
+    int j;
+    int number;
+    for ( i = 0 ; i < strlen(numberToDisplay) ; ++i ){
+        number = numberToDisplay[i]-48;//asci to integer
+        switch(number){
+            case 0:
+                drawZero(size, i);
+                break;
+            case 1:
+                drawOne(size, i);
+                break;
+            case 2:
+                drawTwo(size, i);
+                break;
+            case 3:
+                drawThree(size, i);
+                break;
+            case 4:
+                drawFour(size, i);
+                break;
+            case 5:
+                drawFive(size, i);
+                break;
+            case 6:
+                drawSix(size, i);
+                break;
+            case 7:
+                drawSeven(size, i);
+                break;
+            case 8:
+                drawEight(size, i);
+                break;
+            case 9:
+                drawNine(size, i);
+                break;
+            default:;
+        }
+        
+    }
+    printf("debug>>>\n");
+    for (i=0 ; i < 20 ; ++i){
+        for (j = 0 ; j <30 ; ++j){
+            printf("%c",gLedPannel[i][j]);
+        }
+        printf("\n");
+    }
+    
+}
+
+int main(void){
+    
+    int size;
+    char numberToDisplay[16]={0,};
+    printf("LCD display\n");
+    while ( 1 ){
+        
+        initLed( );
+        //printf("%d %d",'1'-48, '2'-48 );
+        if( receiveInputToDisplay( &size, numberToDisplay ) < 0 )
+        {
+            printf("terminate program\n");
+            return 0;
+        }
+        
+        drawNumbersToLed(size, numberToDisplay);
+        
+        
+        //test
+        //drawZero(size);
+        //drawOne(size);
+        //drawTwo(size);
+        //drawThree(size);
+        //drawFour(size);
+        //drawFive(size);
+        //drawSix(size);
+        //drawSeven(size);
+        //drawEight(size);
+        //drawNine(size);
+    }
+    
+    return 0;
+}
